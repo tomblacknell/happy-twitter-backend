65d1c2fefc15dd84a5f48f11d4cc89ae05e4dcbd
Tom Blacknell <tom.websites@fastmail.co.uk>
2018-04-29 14:44:41 +0100

    Region names file

---
+++ app/region_names.py
@@ -0,0 +1,57 @@
+
+# Region Names
+
+# Authored by Tom Blacknell
+
+# List of valid region names
+# Used to validate region stats endpoint
+
+region_names = [
+    "Glasgow City",
+    "City of Edinburgh",
+    "Highland",
+    "Cardiff",
+    "County Antrim",
+    "London",
+    "Greater Manchester",
+    "West Midlands",
+    "Nottinghamshire",
+    "Lancashire",
+    "Devon",
+    "Staffordshire",
+    "Hampshire",
+    "Tyne and Wear",
+    "Cambridgeshire",
+    "Leicestershire",
+    "Essex",
+    "North Yorkshire",
+    "Cheshire",
+    "Kent",
+    "Hertfordshire",
+    "East Sussex",
+    "Derbyshire",
+    "Surrey",
+    "Somerset",
+    "Norfolk",
+    "Suffolk",
+    "Cornwall",
+    "Oxfordshire",
+    "Buckinghamshire",
+    "West Sussex",
+    "Dorset",
+    "East Riding of Yorkshire",
+    "City of Bristol",
+    "North Somerset",
+    "Shropshire",
+    "Berkshire",
+    "West Yorkshire",
+    "Wiltshire",
+    "County Durham",
+    "Lincolnshire",
+    "Northumberland",
+    "Cumbria",
+    "Bedfordshire",
+    "Herefordshire",
+    "Northamptonshire",
+    "Gloucestershire"
+]
